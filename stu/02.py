dcd89cc8d0fae2d905ac654dab56bb4fef628a3b
luosilent <luosilent@qq.com>
2018-09-13 15:17:55 +0800

    day02

---
+++ stu/02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/13 15:15
+# @Author  : luoSilent
+# @File    : 02.py
+# @Software: PyCharm
+"""
+用户身份验证
+"""
+username = input('请输入用户名: ')
+password = input('请输入口令: ')
+# 如果希望输入口令时 终端中没有回显 可以使用getpass模块的getpass函数
+# import getpass
+# password = getpass.getpass('请输入口令: ')
+if username == 'admin' and password == '123456':
+    print('身份验证成功!')
+else:
+    print('身份验证失败!')
