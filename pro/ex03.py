4229f444d51c6723a85cca365fbd63c204986ab7
luosilent <luosilent@qq.com>
2018-09-13 16:14:02 +0800

    day02

---
+++ pro/ex03.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/13 15:59
+# @Author  : luoSilent
+# @File    : ex03.py
+# @Software: PyCharm
+
+"""
+英制单位英寸和公制单位厘米互换
+"""
+value = float(input('请输入长度: '))
+unit = input('请输入单位: ')
+if unit == 'in' or unit == '英寸':
+    print('%f英寸 = %f厘米' % (value, value * 2.54))
+elif unit == 'cm' or unit == '厘米':
+    print('%f厘米 = %f英寸' % (value, value / 2.54))
+else:
+    print('请输入有效的单位')
+
+
+"""
+分段函数求值
+
+        3x - 5  (x > 1)
+f(x) =  x + 2   (-1 <= x <= 1)
+        5x + 3  (x < -1)
+
+"""
+
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print('f(%.2f) = %.2f' % (x, y))
